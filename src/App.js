4a3a1fc6c6157c22c70bf45f42e8dad9464717e7
emilMircea <emil-mircea@protonmail.com>
2018-10-18 02:07:12 +0300

    move nav-links into sidebar component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
 import React, { Component } from "react";
-import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 import "animate.css";
 import "./App.css";
 import Nav from './components/Nav'
 import Content from './components/Content'
-
-
-
-const ANavLink = ({ children, ...props }) => (
-  <NavLink activeClassName='selected' {...props}>
-    {children}
-  </NavLink>
-)
-
-const Links = (props) => (
-  <div className="Links animated bounce">
-    <ul className="is-flex" style={{ justifyContent: "space-evenly" }}>
-      <li>
-        <ANavLink to='/auth/'>Auth</ANavLink>
-      </li>
-      <li>
-        <ANavLink to="/data">Data</ANavLink>
-      </li>
-      <li>
-        <ANavLink to="/ecomm">E-commerce</ANavLink>
-      </li>
-    </ul>
-  </div>
-);
+import SideBar from './components/Sidebar'
 
 class App extends Component {
   render() {
@@ -35,7 +11,7 @@ class App extends Component {
       <div className="wrapper">
         <div className="grid-container">
           <Nav />
-          <Links />
+          <SideBar />
           <Content />
         </div>
       </div>
