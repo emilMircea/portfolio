8e1fce0cecfa6f89fe6e5abf2e3e0ff3cc15bcb7
emilMircea <emil-mircea@protonmail.com>
2018-10-18 02:08:37 +0300

    add sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const ANavLink = ({ children, ...props }) => (
+  <NavLink activeClassName="selected" {...props}>
+    {children}
+  </NavLink>
+);
+
+const SideBar = () => (
+  <div className="Links animated bounce">
+    <p className='p-heading'>Case Studies</p>
+    <ul className="is-flex ul-links" style={{ justifyContent: "space-evenly" }}>
+      <li>
+        <ANavLink to="/auth/">Auth</ANavLink>
+      </li>
+      <li>
+        <ANavLink to="/data">Data</ANavLink>
+      </li>
+      <li>
+        <ANavLink to="/ecomm">E-commerce</ANavLink>
+      </li>
+    </ul>
+  </div>
+);
+
+export default SideBar;
