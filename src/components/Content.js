9a385ff80ef86d86d24c5a85147d892377c25f3f
emilMircea <emil-mircea@protonmail.com>
2018-10-19 10:09:51 +0300

    add div that holds name and heading

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,16 @@
 import React from "react";
 import {Route} from 'react-router-dom'
 import routes from '../constants/routes'
+import name from '../images/name.svg'
 
 const Content = () => (
-  <div className="Content animated bounce">
+  <div className="Content animated fadeInRight">
+    <div className="content-heading-wrapper">
+      <img className='content-img' src={name} alt="Emil Dragan"/>
+      <p className='content-heading'>
+        <a>More than 3 years of experience with JS and Ruby. <span className='underline'>See CV</span></a>
+      </p>
+    </div>
     {routes.map((route, index) => (
           <Route
             key={index}
