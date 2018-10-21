fd23b99962aaeb0e9999a44505c762b82a714faf
emilMircea <emil-mircea@protonmail.com>
2018-10-22 00:05:10 +0300

    add contact dropdown component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import '../App.css'
-
-const Contact = () => (
-  <div className="Contact animated bounce">
-    <a className="is-text">Contact</a>
-  </div>
-);
+import React from "react";
+import "../App.css";
 
 const Logo = () => (
-  <div className="Logo animated bounce is-flex" style={{ paddingLeft: "3rem" }}>
+  <div
+    className="Logo is-flex dropdown is-active"
+    style={{ paddingLeft: "3rem" }}
+  >
     <a className="is-text">Portfolio</a>
   </div>
 );
+
+class Contact extends React.Component {
+  state = {
+    isActive: false,
+  };
+
+  toggleState = () => {
+    this.setState(prevState => ({
+      isActive: !prevState.isActive
+    }));
+  };
+
+  render() {
+    const { isActive } = this.state;
+    return (
+      <div className={`dropdown Contact ${isActive ? 'is-active' : ''}`}>
+        <div className="dropdown-trigger">
+          <a
+            className="is-text"
+            aria-haspopup="true"
+            aria-controls="dropdown-menu"
+            onClick={this.toggleState}
+          >
+            <span>Contact</span>
+            <span className="icon is-small">
+              <i className="fas fa-angle-down" aria-hidden="true" />
+            </span>
+          </a>
+        </div>
+        <div className="dropdown-menu" id="dropdown-menu" role="menu">
+          <div className="dropdown-content is-flex">
+            <a className="dropdown-item">
+              emil-mircea@protonmail.com
+            </a>
+            <a className="dropdown-item">Github</a>
+
+            <hr className="dropdown-divider" />
+            <a className="dropdown-item">
+              LinkedIn
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 const Nav = () => (
   <React.Fragment>
