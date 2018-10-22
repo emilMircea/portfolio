ab0a604320bb8549ea505210ca42e94bffadf411
emilMircea <emil-mircea@protonmail.com>
2018-10-22 16:47:51 +0300

    add component for references

---
+++ src/components/CV/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Profile = () => (
+  <div className="column is-8 is-offset-2">
+    <p className="subtitle has-text-weight-light cv-heading">Profile</p>
+    <p className='has-text-weight-light'>
+      A full stack developer with 4 years of experience building webapps with JS and Ruby.
+      I estimate spending 40% of the time developing backend APIs with Rails and 60% of the time
+      building UI’s with React. I have experience in business analysis and enjoy finding
+      profitable software solutions to business problems.
+    </p>
+  </div>
+);
+
+export default Profile;
