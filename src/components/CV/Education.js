0d8fb814b0315f23b2516868f2b90132683dfe43
emilMircea <emil-mircea@protonmail.com>
2018-10-22 16:47:24 +0300

    add component for education

---
+++ src/components/CV/Education.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Education = () => (
+  <React.Fragment>
+    <p className="has-text-weight-light is-capitalized">
+      2016: Master of science in public administration, leiden university, the
+      netherlands
+    </p>
+    <p className="has-text-weight-light is-capitalized">
+      2013: Bachelor of Science in public management, The hague university, the
+      netherlands
+    </p>
+  </React.Fragment>
+);
+
+export default Education;
