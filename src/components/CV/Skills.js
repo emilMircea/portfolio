87d7532bceffa7044139e67778274e80c9a3937a
emilMircea <emil-mircea@protonmail.com>
2018-10-22 16:48:51 +0300

    add component for skills

---
+++ src/components/CV/Skills.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Skills = () => (
+  <div className="column is-8 is-offset-2">
+    <p className="subtitle has-text-weight-light cv-heading">Skills</p>
+    <p className='has-text-weight-light'>
+      Languages: Ruby, Javascript
+    </p>
+    <p className='has-text-weight-light'>
+      Frameworks: React, Ruby on Rails
+    </p>
+    <p className='has-text-weight-light'>
+      Databases: MySQL, Postgresql
+    </p>
+    <p className='has-text-weight-light'>
+      Methodologies: Agile, TDD
+    </p>
+  </div>
+);
+
+
+export default Skills;
