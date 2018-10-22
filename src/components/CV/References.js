c6d98c98dee4d4e245d4748a5af67580e71356e2
emilMircea <emil-mircea@protonmail.com>
2018-10-22 16:48:36 +0300

    add component for references

---
+++ src/components/CV/References.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const References = () => (
+  <React.Fragment>
+    <p className="has-text-weight-light is-capitalized">
+      Florin Toader - CTO @ Achiral software.{" "}
+      <span className="is-lowercase">office@achiral.io</span>
+    </p>
+    <p className="has-text-weight-light is-capitalized">
+      Gianna Cipollone - Owner @ Meow Stores.{" "}
+      <span className="is-lowercase">info@meowstores.nl</span>
+    </p>
+    <p className="has-text-weight-light is-capitalized">
+      Mihai Florescu - Manager @ Gloria Jeans.{" "}
+      <span className="is-lowercase">mflorescu@gloriajeans.ro</span>
+    </p>
+  </React.Fragment>
+);
+
+export default References;
