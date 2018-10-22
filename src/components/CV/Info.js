ba7be0de8fe687a1d7904b345d463a2c6fe0cde6
emilMircea <emil-mircea@protonmail.com>
2018-10-22 16:47:36 +0300

    add component with contact info

---
+++ src/components/CV/Info.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Info = () => (
+  <div className="column contact-info is-half is-offset-one-quarter has-text-centered">
+    <p>address: Carrer de Milà i Fontanals, 51, Barcelona, Spain</p>
+    <p>email: emil-mircea@protonmail.com</p>
+  </div>
+);
+
+export default Info ;
