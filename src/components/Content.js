41b163a5ac7b027dc8b2ef6ab984035a0e18f6ff
emilMircea <emil-mircea@protonmail.com>
2018-10-22 12:30:27 +0300

    add link to navigate to CV

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,13 +2,21 @@ import React from "react";
 import {Route} from 'react-router-dom'
 import routes from '../constants/routes'
 import name from '../images/name.svg'
+import { Link } from 'react-router-dom'
+import CVComponent from './CVComponent'
 
 const Content = () => (
   <div className="Content animated fadeInRight">
     <div className="content-heading-wrapper">
       <img className='content-img' src={name} alt="Emil Dragan"/>
       <p className='content-heading'>
-        <a>More than 3 years of experience with JS and Ruby. <span className='underline'>See CV</span></a>
+        <a>More than 3 years of experience with JS and Ruby.
+          <span className='underline'>
+            <Link to='/cv'>
+              See CV
+            </Link>
+          </span>
+        </a>
       </p>
     </div>
     {routes.map((route, index) => (
