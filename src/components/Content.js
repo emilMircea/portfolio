802f8be19c9003ae993bfb61b4d18880bd440634
emilMircea <emil-mircea@protonmail.com>
2018-10-23 11:37:11 +0300

    add prettier code formatting

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,32 +1,28 @@
 import React from "react";
-import {Route} from 'react-router-dom'
-import routes from '../constants/routes'
-import name from '../images/name.svg'
-import { Link } from 'react-router-dom'
-import CVComponent from './CV/CVComponent'
+import { Route } from "react-router-dom";
+import routes from "../constants/routes";
+import name from "../images/name.svg";
+import { Link } from "react-router-dom";
 
 const Content = () => (
   <div className="Content animated fadeInRight">
     <div className="content-heading-wrapper">
-      <img className='content-img' src={name} alt="Emil Dragan"/>
-      <p className='content-heading'>
-        <a>More than 3 years of experience with JS and Ruby.
-          <span className='underline'>
-            <Link to='/cv'>
-              See CV
-            </Link>
-          </span>
-        </a>
+      <img className="content-img" src={name} alt="Emil Dragan" />
+      <p className="content-heading">
+        <Link to="/cv">
+          More than 3 years of experience with JS and Ruby.
+          <span className="underline">See CV</span>
+        </Link>
       </p>
     </div>
     {routes.map((route, index) => (
-          <Route
-            key={index}
-            path={route.path}
-            exact={route.exact}
-            component={route.main}
-          />
-        ))}
+      <Route
+        key={index}
+        path={route.path}
+        exact={route.exact}
+        component={route.main}
+      />
+    ))}
   </div>
 );
 
--- src/components/EcommCase.js
+++ src/components/EcommCase.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const EcommCase = () => (
   <p className='animated fadeIn'>
-    React solution for E-Commerce. State management with MobX, authentication with JWT, forms with Formik. Fast responsive e-comm solution that can be easily adapted to any company that wants to sell products online.
+    React solution for E-Commerce. State management with MobX, authentication with JWT, forms with Formik. Fast responsive e-commerce solution that can be easily adapted to any company that wants to sell products online.
   </p>
 );
 
