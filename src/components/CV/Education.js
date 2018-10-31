bc79cb419a0bd2201142dc49c0f7cda3c6cb317e
emilMircea <emil-mircea@protonmail.com>
2018-10-31 12:46:13 +0100

    add prettier

--- src/components/CV/Education.js
+++ src/components/CV/Education.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 const Education = () => (
   <React.Fragment>
+    <p className="subtitle cv-heading">Education</p>
     <p className="has-text-weight-light is-capitalized">
-      2016: Master of science in public administration, leiden university, the
-      netherlands
+      2016: Master of science in public administration, Leiden University, the
+      Netherlands
     </p>
     <p className="has-text-weight-light is-capitalized">
-      2013: Bachelor of Science in public management, The hague university, the
-      netherlands
+      2013: Bachelor of Science in public management, The Hague University, the
+      Netherlands
     </p>
+    <br/>
   </React.Fragment>
 );
 
--- src/components/CV/Profile.js
+++ src/components/CV/Profile.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 const Profile = () => (
-  <div className="column is-6 is-offset-3">
-    <p className="subtitle has-text-weight-light cv-heading">Profile</p>
+  <div className="column is-8 is-offset-2">
+    <p className="subtitle cv-heading">Profile</p>
     <p className='has-text-weight-light'>
-      A full stack developer with 4 years of experience building webapps with JS and Ruby.
-      I estimate spending 40% of the time developing backend APIs with Rails and 60% of the time
-      building UI’s with React. I have experience in business analysis and enjoy finding
-      profitable software solutions to business problems.
+      A full stack developer with experience as a business analyst and 3+ years of experience building webapps with JS and Ruby. I estimate spending 40% of the time developing backend APIs with Rails and 60% of the time
+      building UI’s with React. I enjoy finding profitable software solutions to business problems.
     </p>
   </div>
 );
