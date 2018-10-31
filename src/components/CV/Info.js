903854b3968f281415cfbb3809aa52acccd12169
emilMircea <emil-mircea@protonmail.com>
2018-10-31 12:45:34 +0100

    add mobile number

--- src/components/CV/Info.js
+++ src/components/CV/Info.js
@@ -4,6 +4,7 @@ const Info = () => (
   <div className="column contact-info is-half is-offset-one-quarter has-text-centered">
     <p>address: Carrer de Milà i Fontanals, 51, Barcelona, Spain</p>
     <p>email: emil-mircea@protonmail.com</p>
+    <p>mobile: +34 615 248 763</p>
   </div>
 );
 
