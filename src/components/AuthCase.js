33b1f2e2cd7a6f35d2dff092bbb53e0124b8df36
emilMircea <emil-mircea@protonmail.com>
2018-10-31 16:56:51 +0100

    add title for each case study

--- src/components/AuthCase.js
+++ src/components/AuthCase.js
@@ -2,6 +2,9 @@ import React from "react";
 
 const AuthCase = () => (
   <div className="animated fadeIn case-wrapper">
+    <p className="subtitle has-text-grey is-uppercase has-text-weight-semibold">
+      Authentication in Redux - Case Study
+    </p>
     <p>
       Redux solution for authentication that offers access to protected routes.
       Uses react-router 4 as router, redux-saga for async requests and formik
--- src/components/DataCase.js
+++ src/components/DataCase.js
@@ -3,6 +3,9 @@ import dashboard from "../images/data_analysis.png";
 
 const DataCase = () => (
   <div className="animated fadeIn case-wrapper">
+    <p className="subtitle has-text-grey is-uppercase has-text-weight-semibold">
+      Data Analysis Case Study
+    </p>
     <p>
       React solution for client-side Data Analysis. Makes possible uploading
       excel files and constructing charts to identify relevant trends. Below a
