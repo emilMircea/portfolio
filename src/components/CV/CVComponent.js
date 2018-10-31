5648b71f166f702ddf14ec30edc649efa5046743
emilMircea <emil-mircea@protonmail.com>
2018-10-31 12:48:01 +0100

    add new components import and switch order of sections

--- src/components/CV/CVComponent.js
+++ src/components/CV/CVComponent.js
@@ -1,22 +1,26 @@
 import React from "react";
 import "./CV.css";
-import Info from './Info'
-import Profile from './Profile'
-import Skills from './Skills'
-import WorkHistory from './WorkHistory'
+import Info from "./Info";
+import Profile from "./Profile";
+import Skills from "./Skills";
+import Employment from "./Employment";
+import Education from "./Education";
+import References from "./References";
 
 const CVComponent = () => (
-
-    <div className="columns is-multiline">
-      <div className="column is-6 is-offset-3 has-text-centered cv-column">
-        <p className="title">Emil Dragan</p>
+  <div className="columns is-multiline">
+    <div className="column is-8 is-offset-2 cv-column">
+      <div className="title is-flex has-text-centered ">
+        <p className="is-uppercase">Emil</p>
+        <p className="is-uppercase">Dragan</p>
       </div>
       <Info />
-      <Profile />
+      <Employment />
       <Skills />
-      <WorkHistory />
+      <Education />
+      <References />
     </div>
-
+  </div>
 );
 
 export default CVComponent;
