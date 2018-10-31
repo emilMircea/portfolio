f4ab32649592ba90fb3069ddc27b9d634b1a9a67
emilMircea <emil-mircea@protonmail.com>
2018-10-31 12:46:31 +0100

    add prettier

--- src/components/EcommCase.js
+++ src/components/EcommCase.js
@@ -4,7 +4,7 @@ const EcommCase = () => (
   <div className='animated fadeIn case-wrapper'>
     <p className="subtitle has-text-grey is-uppercase has-text-weight-semibold">E-commerce Case Study</p>
     <p>
-      React-MobX solution for E-Commerce. Fast responsive e-commerce solution that can
+      React-MobX solution for E-Commerce. Fast, responsive e-commerce solution that can
       be easily adapted to any company that wants to sell products online.
     </p>
   </div>
