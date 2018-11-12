25c0df17a65ef67c8ce1a85cc4c0a9469b84d4b7
emilMircea <emil-mircea@protonmail.com>
2018-11-12 19:01:03 +0100

    remove full address in favor of just city and country

--- src/components/CV/Info.js
+++ src/components/CV/Info.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Info = () => (
   <div className="column contact-info is-half is-offset-one-quarter has-text-centered">
-    <p>address: Carrer de Milà i Fontanals, 51, Barcelona, Spain</p>
+    <p>address: Barcelona, Spain</p>
     <p>email: emil-mircea@protonmail.com</p>
     <p>mobile: +34 615 248 763</p>
   </div>
