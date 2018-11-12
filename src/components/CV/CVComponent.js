8fdcd92118dd54522e0604502c2d742557225845
emilMircea <emil-mircea@protonmail.com>
2018-11-12 19:16:31 +0100

    add styled wrapper and header slogan tile component

--- src/components/CV/CVComponent.js
+++ src/components/CV/CVComponent.js
@@ -5,6 +5,33 @@ import Skills from "./Skills";
 import Employment from "./Employment";
 import Education from "./Education";
 import References from "./References";
+import styled from "styled-components";
+
+const HeaderSloganWrapper = styled.article`
+  background: #363636c7;
+  padding: 0.5rem 1rem;
+  text-transform: uppercase;
+  color: whitesmoke;
+`;
+
+const HeadingTile = () => (
+  <div className="tile is-ancestor">
+    <div className="tile is-vertical">
+      <div className="tile">
+        <div
+          className="tile is-parent is-vertical"
+          style={{ marginTop: "-1rem" }}
+        >
+          <HeaderSloganWrapper className="tile is-child notification has-text-centered">
+            <p className="is-size-6 has-text-weight-light is-italic">
+              More than 4 years of experience with Ruby and Javascript
+            </p>
+          </HeaderSloganWrapper>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 const CVComponent = () => (
   <div className="columns is-multiline">
@@ -13,6 +40,7 @@ const CVComponent = () => (
         <p className="is-uppercase">Emil</p>
         <p className="is-uppercase">Dragan</p>
       </div>
+      <HeadingTile />
       <Info />
       <Employment />
       <Skills />
