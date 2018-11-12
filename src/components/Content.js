0e80c3b49e5057b7164e0074424fe6f40f741b0d
emilMircea <emil-mircea@protonmail.com>
2018-11-12 10:18:20 +0100

    replace Link with NavLink

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,18 +1,17 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, NavLink } from "react-router-dom";
 import routes from "../constants/routes";
-import name from "../images/name.svg";
-import { Link } from "react-router-dom";
+import name from "../images/name2.svg";
 
 const Content = () => (
   <div className="Content animated fadeInRight">
     <div className="content-heading-wrapper">
       <img className="content-img" src={name} alt="Emil Dragan" />
       <p className="content-heading">
-        <Link to="/cv">
+        <NavLink className='cv-link' to="/cv">
           More than 3 years of experience with JS and Ruby.
           <span className="underline">See CV</span>
-        </Link>
+        </NavLink>
       </p>
     </div>
     {routes.map((route, index) => (
