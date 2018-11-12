912315f264ba3afc807927b022c65f6fb7793574
emilMircea <emil-mircea@protonmail.com>
2018-11-12 10:17:56 +0100

    add css class and prettier format

--- src/components/CV/Skills.js
+++ src/components/CV/Skills.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const Skills = () => (
-  <div className="column">
+  <div className="column skills">
     <p className="subtitle cv-heading">Skills</p>
-    <p className="has-text-weight-light">Languages: Ruby, Javascript</p>
+    <p className="has-text-weight-light">Languages: Ruby, Javascript / ES6, HTML, CSS</p>
     <p className="has-text-weight-light">Frameworks: React, Ruby on Rails</p>
     <p className="has-text-weight-light">Databases: MySQL, Postgresql</p>
     <p className="has-text-weight-light">Methodologies: Agile, TDD</p>
