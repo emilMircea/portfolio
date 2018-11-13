c1caddd4224cea7c3595db83d5d24923f005bcd0
emilMircea <emil-mircea@protonmail.com>
2018-11-13 11:09:03 +0100

    remove NavLink since not used

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, NavLink } from "react-router-dom";
+import { Route } from "react-router-dom";
 import routes from "../constants/routes";
 import name from "../images/name2.svg";
 
