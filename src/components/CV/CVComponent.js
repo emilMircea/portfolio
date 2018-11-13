67cb833942f56a8e95d6b7ec8c38fc3bcdcca877
emilMircea <emil-mircea@protonmail.com>
2018-11-13 12:41:58 +0100

    add fixed link to go back to portfolio page also styles for said link

--- src/components/CV/CVComponent.js
+++ src/components/CV/CVComponent.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./CV.css";
 import Info from "./Info";
 import Skills from "./Skills";
 import Employment from "./Employment";
 import Education from "./Education";
 import References from "./References";
+import { IoIosArrowBack } from "react-icons/io";
 import styled from "styled-components";
 
 const HeaderSloganWrapper = styled.article`
@@ -35,6 +37,12 @@ const HeadingTile = () => (
 
 const CVComponent = () => (
   <div className="columns is-multiline">
+    <Link className="back-to-root-link" to="/">
+      <span className="icon">
+        <IoIosArrowBack />
+      </span>
+      Back to portfolio page
+    </Link>
     <div className="column is-8 is-offset-2 cv-column">
       <div className="title is-flex has-text-centered ">
         <p className="is-uppercase">Emil</p>
