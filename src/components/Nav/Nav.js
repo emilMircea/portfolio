226578b560b0948420bcbf747527e321eab9a197
emilMircea <emil-mircea@protonmail.com>
2018-11-16 16:01:50 +0100

    replace react-icons with custom icons in the images folder also remove react-icons due to size!

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { IoIosPhonePortrait } from "react-icons/io";
-import { IoIosMail } from "react-icons/io";
-import { IoLogoGithub } from "react-icons/io";
+import styled from 'styled-components'
+// icons
+import phoneIcon from '../../images/phone-icon.svg'
+import mailIcon from '../../images/mail-icon.svg'
+import githubIcon from '../../images/github-icon.svg'
 import "./Nav.css";
+
+const IconImage = styled.img`
+  width: 1rem;
+  height: 1rem;
+  margin-right: 8px;
+`;
 
 const Logo = () => (
   <div
@@ -27,48 +35,40 @@ class Contact extends React.Component {
 
   render() {
     const { isActive } = this.state;
-    return (
-      <div className="Contact">
+    return <div className="Contact">
         <Link className="see-cv-link" to="/cv">
           See CV
         </Link>
         <div className={`dropdown ${isActive ? "is-active" : ""}`}>
           <div className="dropdown-trigger has-text-centered">
-            <p
-              className="see-contact-info is-text"
-              aria-haspopup="true"
-              aria-controls="dropdown-menu"
-              onClick={this.toggleState}
-              style={{ marginTop: "-2px" }}
-            >
+            <p className="see-contact-info is-text" aria-haspopup="true" aria-controls="dropdown-menu" onClick={this.toggleState} style={{ marginTop: "-2px" }}>
               Contact Info
             </p>
           </div>
           <div className="dropdown-menu" id="dropdown-menu" role="menu">
             <div className="dropdown-content">
               <p className="dropdown-item">
-              <span className="icon">
-                <IoIosMail />
-              </span>
+                <span className="icon">
+                  <IconImage src={mailIcon} alt="email icon" />
+                </span>
                 <a href="#null">emil-mircea@protonmail.com</a>
               </p>
               <p className="dropdown-item">
                 <span className="icon">
-                  <IoIosPhonePortrait />
+                  <IconImage src={phoneIcon} alt="phone icon" />
                 </span>
                 <a href="#null">+34 615 248 763</a>
               </p>
               <p className="dropdown-item">
                 <span className="icon">
-                  <IoLogoGithub />
+                <IconImage src={githubIcon} alt='github icon' />
                 </span>
                 <a href="https://github.com/emilMircea">Github Profile</a>
               </p>
             </div>
           </div>
         </div>
-      </div>
-    );
+      </div>;
   }
 }
 
